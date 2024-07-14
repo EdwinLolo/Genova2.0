44e4fa456ac275933936ada2f42531f8ab04a652
owencahyadi <owenchristian123@gmail.com>
2024-07-14 11:50:08 +0700

    Revisi Eulympic

--- laravel-react/resources/js/Components/Rangkaian/Eulympic/EulympicPhoto.jsx
+++ laravel-react/resources/js/Components/Rangkaian/Eulympic/EulympicPhoto.jsx
@@ -4,6 +4,14 @@ import LombaEulympic from "../../../Assets/Rangkaian/Eulympic/Eulimpic_Asset_3_P
 import Valorant from "../../../Assets/Rangkaian/Eulympic/valorant.webp";
 import MoleBaru from "../../../Assets/Rangkaian/Eulympic/molelogo.webp";
 import IU from "../../../Assets/Rangkaian/Eulympic/iu.webp";
+import Lomba1 from "../../../Assets/Rangkaian/Eulympic/Lomba1.jpg";
+import Lomba2 from "../../../Assets/Rangkaian/Eulympic/Lomba2.jpg";
+import Lomba3 from "../../../Assets/Rangkaian/Eulympic/Lomba3.jpg";
+import Lomba4 from "../../../Assets/Rangkaian/Eulympic/Lomba4.jpg";
+import Lomba5 from "../../../Assets/Rangkaian/Eulympic/Lomba5.jpg";
+import Lomba6 from "../../../Assets/Rangkaian/Eulympic/Lomba6.jpg";
+import Lomba7 from "../../../Assets/Rangkaian/Eulympic/Lomba7.jpg";
+import Lomba8 from "../../../Assets/Rangkaian/Eulympic/Lomba8.jpg";
 import vicmic from "../../../Assets/HomeAssets/Sponsor/Vicmic.webp";
 import lenovo from "../../../Assets/HomeAssets/Sponsor/lenovo.webp";
 
@@ -24,98 +32,98 @@ function EulympicPhoto() {
         return () => window.removeEventListener("resize", handleResize);
     }, []);
 
-    const [countdownML, setCountdownML] = useState({
-        days: "00",
-        hours: "00",
-        minutes: "00",
-        seconds: "00",
-    });
+    // const [countdownML, setCountdownML] = useState({
+    //     days: "00",
+    //     hours: "00",
+    //     minutes: "00",
+    //     seconds: "00",
+    // });
     
-    const [countdownValorant, setCountdownValorant] = useState({
-        days: "00",
-        hours: "00",
-        minutes: "00",
-        seconds: "00",
-    });
+    // const [countdownValorant, setCountdownValorant] = useState({
+    //     days: "00",
+    //     hours: "00",
+    //     minutes: "00",
+    //     seconds: "00",
+    // });
     
-    const [isFormMLAvailable, setIsFormMLAvailable] = useState(false);
-    const [isFormValAvailable, setIsFormValAvailable] = useState(false);
+    // const [isFormMLAvailable, setIsFormMLAvailable] = useState(false);
+    // const [isFormValAvailable, setIsFormValAvailable] = useState(false);
     
-    useEffect(() => {
-        const targetDateML = new Date(2024, 4, 3); // Emel sbatesnya sampe 1 mei 2024
-        const targetDateValorant = new Date(2024, 3, 24); // Valorant sampe 24 April 2024
+    // useEffect(() => {
+    //     const targetDateML = new Date(2024, 4, 3); // Emel sbatesnya sampe 1 mei 2024
+    //     const targetDateValorant = new Date(2024, 3, 24); // Valorant sampe 24 April 2024
     
-        // ML Countdown
-        const timerML = setInterval(() => {
-            const currentTime = new Date();
-            const diffML = targetDateML - currentTime;
+    //     // ML Countdown
+    //     const timerML = setInterval(() => {
+    //         const currentTime = new Date();
+    //         const diffML = targetDateML - currentTime;
         
-            if (diffML <= 0) {
-                clearInterval(timerML);
-                setCountdownML({
-                    days: "00",
-                    hours: "00",
-                    minutes: "00",
-                    seconds: "00",
-                });
-                setIsFormMLAvailable(true);
-                return;
-            }
+    //         if (diffML <= 0) {
+    //             clearInterval(timerML);
+    //             setCountdownML({
+    //                 days: "00",
+    //                 hours: "00",
+    //                 minutes: "00",
+    //                 seconds: "00",
+    //             });
+    //             setIsFormMLAvailable(true);
+    //             return;
+    //         }
         
-                updateCountdownML(diffML);
-        }, 1000);
+    //             updateCountdownML(diffML);
+    //     }, 1000);
     
-        // Valorant Countdown
-        const timerValorant = setInterval(() => {
-            const currentTime = new Date();
-            const diffValorant = targetDateValorant - currentTime;
+    //     // Valorant Countdown
+    //     const timerValorant = setInterval(() => {
+    //         const currentTime = new Date();
+    //         const diffValorant = targetDateValorant - currentTime;
     
-            if (diffValorant <= 0) {
-                clearInterval(timerValorant);
-                setCountdownValorant({
-                    days: "00",
-                    hours: "00",
-                    minutes: "00",
-                    seconds: "00",
-                });
-            setIsFormValAvailable(true);
-            return;
-            }
+    //         if (diffValorant <= 0) {
+    //             clearInterval(timerValorant);
+    //             setCountdownValorant({
+    //                 days: "00",
+    //                 hours: "00",
+    //                 minutes: "00",
+    //                 seconds: "00",
+    //             });
+    //         setIsFormValAvailable(true);
+    //         return;
+    //         }
     
-            updateCountdownValorant(diffValorant);
-        }, 1000);
-        return () => {
-            clearInterval(timerML);
-            clearInterval(timerValorant);
-        };
-    }, []);
-    function updateCountdownML(diffML) {
-        const days = Math.floor(diffML / (1000 * 60 * 60 * 24));
-        const hours = Math.floor((diffML / (1000 * 60 * 60)) % 24);
-        const minutes = Math.floor((diffML / 1000 / 60) % 60);
-        const seconds = Math.floor((diffML / 1000) % 60);
+    //         updateCountdownValorant(diffValorant);
+    //     }, 1000);
+    //     return () => {
+    //         clearInterval(timerML);
+    //         clearInterval(timerValorant);
+    //     };
+    // }, []);
+    // function updateCountdownML(diffML) {
+    //     const days = Math.floor(diffML / (1000 * 60 * 60 * 24));
+    //     const hours = Math.floor((diffML / (1000 * 60 * 60)) % 24);
+    //     const minutes = Math.floor((diffML / 1000 / 60) % 60);
+    //     const seconds = Math.floor((diffML / 1000) % 60);
     
-        setCountdownML({
-            days: days < 10 ? "0" + days : days.toString(),
-            hours: hours < 10 ? "0" + hours : hours.toString(),
-            minutes: minutes < 10 ? "0" + minutes : minutes.toString(),
-            seconds: seconds < 10 ? "0" + seconds : seconds.toString(),
-        });
-    }
+    //     setCountdownML({
+    //         days: days < 10 ? "0" + days : days.toString(),
+    //         hours: hours < 10 ? "0" + hours : hours.toString(),
+    //         minutes: minutes < 10 ? "0" + minutes : minutes.toString(),
+    //         seconds: seconds < 10 ? "0" + seconds : seconds.toString(),
+    //     });
+    // }
     
-    function updateCountdownValorant(diffValorant) {
-        const days = Math.floor(diffValorant / (1000 * 60 * 60 * 24));
-        const hours = Math.floor((diffValorant / (1000 * 60 * 60)) % 24);
-        const minutes = Math.floor((diffValorant / 1000 / 60) % 60);
-        const seconds = Math.floor((diffValorant / 1000) % 60);
+    // function updateCountdownValorant(diffValorant) {
+    //     const days = Math.floor(diffValorant / (1000 * 60 * 60 * 24));
+    //     const hours = Math.floor((diffValorant / (1000 * 60 * 60)) % 24);
+    //     const minutes = Math.floor((diffValorant / 1000 / 60) % 60);
+    //     const seconds = Math.floor((diffValorant / 1000) % 60);
     
-        setCountdownValorant({
-            days: days < 10 ? "0" + days : days.toString(),
-            hours: hours < 10 ? "0" + hours : hours.toString(),
-            minutes: minutes < 10 ? "0" + minutes : minutes.toString(),
-            seconds: seconds < 10 ? "0" + seconds : seconds.toString(),
-        });
-    }
+    //     setCountdownValorant({
+    //         days: days < 10 ? "0" + days : days.toString(),
+    //         hours: hours < 10 ? "0" + hours : hours.toString(),
+    //         minutes: minutes < 10 ? "0" + minutes : minutes.toString(),
+    //         seconds: seconds < 10 ? "0" + seconds : seconds.toString(),
+    //     });
+    // }
 
     if (isWideScreen) {
         return (
@@ -132,92 +140,57 @@ function EulympicPhoto() {
                         style={{
                             // "--width": "300px",
                             // "--height": "500px",
-                            "--quantity": 10,
+                            "--quantity": 4,
                         }}
                     >
                         <div className="list">
                             <div className="item" style={{ "--position": 1 }}>
-                                <img src={IU} alt="IU" />
+                                <img src={Lomba1} alt="IU" />
                             </div>
                             <div className="item" style={{ "--position": 2 }}>
-                                <img src={IU} alt="IU" />
+                                <img src={Lomba2} alt="IU" />
                             </div>
                             <div className="item" style={{ "--position": 3 }}>
-                                <img src={IU} alt="IU" />
+                                <img src={Lomba3} alt="IU" />
                             </div>
                             <div className="item" style={{ "--position": 4 }}>
-                                <img src={IU} alt="IU" />
-                            </div>
-                            <div className="item" style={{ "--position": 5 }}>
-                                <img src={IU} alt="IU" />
-                            </div>
-                            <div className="item" style={{ "--position": 6 }}>
-                                <img src={IU} alt="IU" />
-                            </div>
-                            <div className="item" style={{ "--position": 7 }}>
-                                <img src={IU} alt="IU" />
-                            </div>
-                            <div className="item" style={{ "--position": 8 }}>
-                                <img src={IU} alt="IU" />
-                            </div>
-                            <div className="item" style={{ "--position": 9 }}>
-                                <img src={IU} alt="IU" />
-                            </div>
-                            <div className="item" style={{ "--position": 10 }}>
-                                <img src={IU} alt="IU" />
+                                <img src={Lomba4} alt="IU" />
                             </div>
                         </div>
                     </div>
-
                     <div
                         className="slider"
                         style={{
                             // "--width": "300px",
                             // "--height": "500px",
-                            "--quantity": 9,
+                            "--quantity": 4,
                         }}
                     >
                         <div className="list">
                             <div className="item" style={{ "--position": 1 }}>
-                                <img src={IU} alt="IU" />
+                                <img src={Lomba5} alt="IU" />
                             </div>
                             <div className="item" style={{ "--position": 2 }}>
-                                <img src={IU} alt="IU" />
+                                <img src={Lomba6} alt="IU" />
                             </div>
                             <div className="item" style={{ "--position": 3 }}>
-                                <img src={IU} alt="IU" />
+                                <img src={Lomba7} alt="IU" />
                             </div>
                             <div className="item" style={{ "--position": 4 }}>
-                                <img src={IU} alt="IU" />
-                            </div>
-                            <div className="item" style={{ "--position": 5 }}>
-                                <img src={IU} alt="IU" />
-                            </div>
-                            <div className="item" style={{ "--position": 6 }}>
-                                <img src={IU} alt="IU" />
-                            </div>
-                            <div className="item" style={{ "--position": 7 }}>
-                                <img src={IU} alt="IU" />
-                            </div>
-                            <div className="item" style={{ "--position": 8 }}>
-                                <img src={IU} alt="IU" />
-                            </div>
-                            <div className="item" style={{ "--position": 9 }}>
-                                <img src={IU} alt="IU" />
+                                <img src={Lomba8} alt="IU" />
                             </div>
                             {/* <div className="item" style={{ "--position": 10 }}>
                                 <img src={IU} alt="IU" />
                             </div> */}
                         </div>
                     </div>
-                    <a
-                        href="https://drive.google.com/drive/u/4/folders/1YPxjL2-g2hWcnWfGeBHxLPYoJJl6NQjx"
-                        target="_blank"
-                    >
-                        <button type="button" className="btn1">
-                            See More
-                        </button>
-                    </a>
+                </div>
+                <div className="sponsorHP">
+                    <h1>Sponsored by:</h1>
+                    <div className="logosponsor">
+                        <img src={vicmic} alt="vicmic" />
+                        <img src={lenovo} alt="lenovo" />
+                    </div>
                 </div>
             </div>
         );
@@ -226,7 +199,7 @@ function EulympicPhoto() {
             <div>
                 <img className="LombaPCEulympic" src={LombaEulympic} alt="Lomba Eulympic" />
                 <div className="container-esport flex relative">
-                    <div className="mobilelegend">
+                    {/* <div className="mobilelegend">
                         <div className="esport-img-wrapper-mole">
                             <img className="mole-image" src={MoleBaru} alt="Mole Logo" />
                             <span>Register Here!</span>
@@ -296,6 +269,45 @@ function EulympicPhoto() {
                         >
                         <span className="btn-txt1">FULL</span>
                         </button>
+                    </div> */}
+
+                    <div className="w-full CarouselPhotoPC">
+                        <div
+                            className="sliderPC"
+                            reverse="true"
+                            style={{
+                                // "--width": "300px",
+                                // "--height": "500px",
+                                "--quantity": 8,
+                            }}
+                        >
+                            <div className="listPC">
+                                <div className="itemPC" style={{ "--position": 1 }}>
+                                    <img src={Lomba1} alt="IU" />
+                                </div>
+                                <div className="itemPC" style={{ "--position": 2 }}>
+                                    <img src={Lomba2} alt="IU" />
+                                </div>
+                                <div className="itemPC" style={{ "--position": 3 }}>
+                                    <img src={Lomba3} alt="IU" />
+                                </div>
+                                <div className="itemPC" style={{ "--position": 4 }}>
+                                    <img src={Lomba4} alt="IU" />
+                                </div>
+                                <div className="itemPC" style={{ "--position": 5 }}>
+                                    <img src={Lomba5} alt="IU" />
+                                </div>
+                                <div className="itemPC" style={{ "--position": 6 }}>
+                                    <img src={Lomba6} alt="IU" />
+                                </div>
+                                <div className="itemPC" style={{ "--position": 7 }}>
+                                    <img src={Lomba7} alt="IU" />
+                                </div>
+                                <div className="itemPC" style={{ "--position": 8 }}>
+                                    <img src={Lomba8} alt="IU" />
+                                </div>
+                            </div>
+                        </div>
                     </div>
                 </div>
                 <div className="sponsor">
--- laravel-react/resources/js/Components/Rangkaian/Eulympic/LogoEulympic.jsx
+++ laravel-react/resources/js/Components/Rangkaian/Eulympic/LogoEulympic.jsx
@@ -3,7 +3,7 @@ import LogoEulympicPNG from "../../../Assets/Rangkaian/Eulympic/Eulimpic_Asset1.
 import LogoEulympicPNG2 from "../../../Assets/Rangkaian/Eulympic/Eulimpic_Asset1_head.png";
 import LogoEulympicPNG3 from "../../../Assets/Rangkaian/Eulympic/Eulimpic_Asset1_body.png";
 import LogoEulympicPC from "../../../Assets/Rangkaian/Eulympic/Eulimpic_Asset_1_PC.png";
-import IsiEulympicPC from "../../../Assets/Rangkaian/Eulympic/Eulimpic_Asset_2_PC.png";
+import IsiEulympicPC from "../../../Assets/Rangkaian/Eulympic/Eulimpic_Asset_2_PC.png"
 import "../../Font.css";
 import "./LogoEulympicStyle.css";
 
@@ -23,24 +23,16 @@ function LogoEulympic() {
     if (isWideScreen) {
         return (
             <div>
-                <img
-                    src={LogoEulympicPNG2}
-                    alt="Logo Eulympic"
-                    className="z-10"
-                />
+                <img src={LogoEulympicPNG2} alt="Logo Eulympic" />
                 <div className="relative w-full text-center">
                     <img
                         className="w-full LogoEulympicPNG"
                         src={LogoEulympicPNG3}
                         alt="Logo Eulympic"
                     />
-                    <div className="z-50 info">
-                        <h1>𝐄-𝐔𝐋𝐘𝐌𝐏𝐈𝐂</h1>
-                        <p
-                            style={{
-                                fontFamily: "SanFran-Regular, sans-serif",
-                            }}
-                        >
+                    <div className="info">
+                        <h1 style={{ fontFamily: "Akbaal, sans-serif" }}>E-Ulympic</h1>
+                        <p className="textEulympic" style={{ fontFamily: "SanFran-Regular, sans-serif" }}>
                             𝐄-𝐔𝐋𝐘𝐌𝐏𝐈𝐂 merupakan rangkaian perlombaan pertama
                             dari UMN Festival 2024. E-Ulympic akan
                             mempertandikan dua permainan E-sports yang banyak di
@@ -60,35 +52,26 @@ function LogoEulympic() {
         );
     } else {
         return (
-            <div className="relative flex EulympicPC">
-                <img
-                    className="LogoPCEulympic"
-                    src={LogoEulympicPC}
-                    alt="Logo Eulympic"
-                />
-                <img
-                    className="IsiPCEulympic"
-                    src={IsiEulympicPC}
-                    alt="Isi Eulympic"
-                />
+            <div className="EulympicPC flex relative">
+                <img className="LogoPCEulympic" src={LogoEulympicPC} alt="Logo Eulympic" />
+                <img className="IsiPCEulympic" src={IsiEulympicPC} alt="Isi Eulympic" />
                 <div className="infoPCEulympic">
                     <div className="JudulPCEulympic">
-                        <h1 style={{ fontFamily: "Akbaal, sans-serif" }}>
-                            E-Ulympic
-                        </h1>
+                        <h1 style={{ fontFamily: "Akbaal, sans-serif" }}>E-Ulympic</h1>
                     </div>
                     <p style={{ fontFamily: "SanFran-Regular, sans-serif" }}>
-                        𝐄-𝐔𝐋𝐘𝐌𝐏𝐈𝐂 merupakan rangkaian perlombaan pertama dari
-                        UMN Festival 2024. E-Ulympic akan mempertandikan dua
-                        permainan E-sports yang banyak di gemari yaitu Valorant
-                        dan Mobile Legends.
-                        <br />
-                        <br />
+                        𝐄-𝐔𝐋𝐘𝐌𝐏𝐈𝐂 merupakan rangkaian perlombaan pertama
+                        dari UMN Festival 2024. E-Ulympic akan
+                        mempertandikan dua permainan E-sports yang banyak di
+                        gemari yaitu Valorant dan Mobile Legends.
+                    <br />
+                    <br />
                         𝐄-𝐔𝐋𝐘𝐌𝐏𝐈𝐂 juga memiliki tujuan yaitu menanamkan dan
                         meningkatkan jiwa kompetitif para #Legions melalui
-                        turnamen E-Sports! Diharapkan E-Ulympic dapat menjadi
-                        wadah bagi seluruh mahasiswa/i untuk menunjukan bakat
-                        dan talenta mereka dalam dunia E-Sports!
+                        turnamen E-Sports! Diharapkan E-Ulympic dapat
+                        menjadi wadah bagi seluruh mahasiswa/i untuk
+                        menunjukan bakat dan talenta mereka dalam dunia
+                        E-Sports!
                     </p>
                 </div>
             </div>
