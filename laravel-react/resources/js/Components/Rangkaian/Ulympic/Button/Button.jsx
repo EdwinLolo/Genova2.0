554d817ff2430dbf4c7eed02251b7229ef821fec
Hosea <hoseahose63@gmail.com>
2024-07-15 19:48:05 +0700

    Form ulympic Navbar

--- laravel-react/resources/js/Components/Rangkaian/Ulympic/Button/Button.jsx
+++ laravel-react/resources/js/Components/Rangkaian/Ulympic/Button/Button.jsx
@@ -1,20 +1,35 @@
-import React from "react";
+import React, { useRef, useEffect, useState } from "react";
 import "./Button.css";
 
 const Button = ({ text, onClick, selectedLomba, lomba }) => {
+    const [textWidth, setTextWidth] = useState(0);
+    const textRef = useRef(null);
     const isSelected =
         selectedLomba && selectedLomba.id_lomba === lomba.id_lomba;
+
+    useEffect(() => {
+        if (textRef.current) {
+            setTextWidth(textRef.current.getBBox().width);
+        }
+    }, [text]);
+
+    const svgWidth = Math.max(200, textWidth + 20);
 
     return (
         <div
             className={`svg-wrapper ${isSelected ? "selected" : ""}`}
             onClick={onClick}
         >
-            <svg height="40" width="200" xmlns="http://www.w3.org/2000/svg">
-                <rect id="shape" height="40" width="200" />
+            <svg
+                width={svgWidth}
+                height="40"
+                xmlns="http://www.w3.org/2000/svg"
+            >
+                <rect id="shape" height="40" width={svgWidth} />
                 <text
-                    x="100"
-                    y="25"
+                    ref={textRef}
+                    x="50%"
+                    y="50%"
                     textAnchor="middle"
                     alignmentBaseline="middle"
                     className="button-text"
