57ac9f3896fbf20a90ea088b68d18cc8d964e342
EdwinLolo <edwinfedora@gmail.com>
2024-10-28 21:44:10 +0700

    sponsor ucare

--- laravel-react/resources/js/Components/Rangkaian/Ucare/BodyUcare.jsx
+++ laravel-react/resources/js/Components/Rangkaian/Ucare/BodyUcare.jsx
@@ -5,6 +5,26 @@ import MobileUcare from "../../../Assets/Rangkaian/Ucare/Ucare_Asset_Mobile.webp
 import "../../Font.css";
 import "./BodyUcareStyle.css";
 
+// import logo sponsor
+import CiptaWarna from "../../../Assets/Rangkaian/Ucare/Sponsor_Ucare/CiptaWarna.png";
+import Deorex from "../../../Assets/Rangkaian/Ucare/Sponsor_Ucare/deorex.png";
+import Evolene from "../../../Assets/Rangkaian/Ucare/Sponsor_Ucare/Evolene_Black.png";
+import Asus from "../../../Assets/Rangkaian/Ucare/Sponsor_Ucare/asus.jpg";
+import Fifgroup from "../../../Assets/Rangkaian/Ucare/Sponsor_Ucare/fifgroup.png";
+import ITO_EN from "../../../Assets/Rangkaian/Ucare/Sponsor_Ucare/ITO_EN.png";
+
+import Leminerale from "../../../Assets/Rangkaian/Ucare/Sponsor_Ucare/le_minerale.webp";
+import OCBC from "../../../Assets/Rangkaian/Ucare/Sponsor_Ucare/ocbc.webp";
+import Pocari from "../../../Assets/Rangkaian/Ucare/Sponsor_Ucare/pocari.webp";
+import ROG from "../../../Assets/Rangkaian/Ucare/Sponsor_Ucare/rog.png";
+import Roppang from "../../../Assets/Rangkaian/Ucare/Sponsor_Ucare/roppang.png";
+import Sunpride_LYFE from "../../../Assets/Rangkaian/Ucare/Sponsor_Ucare/sunpride_LYFE.png";
+import Sunpride from "../../../Assets/Rangkaian/Ucare/Sponsor_Ucare/Sunpride.webp";
+import Teh_pucuk from "../../../Assets/Rangkaian/Ucare/Sponsor_Ucare/teh_pucuk.webp";
+import Tenjin from "../../../Assets/Rangkaian/Ucare/Sponsor_Ucare/Tenjin.png";
+import SariRoti from "../../../Assets/Rangkaian/Ucare/Sponsor_Ucare/LOGO-SARI.png";
+import Vicmic from "../../../Assets/Rangkaian/Ucare/Sponsor_Ucare/Vicmic.webp";
+
 function BodyUcare() {
     const [windowWidth, setWindowWidth] = useState(window.innerWidth);
     const isWideScreen = windowWidth <= 1024;
@@ -54,7 +74,7 @@ function BodyUcare() {
                             we join forces to make a positive impact and spread
                             goodwill!"
                         </p>
-                        <div className="flex justify-center w-full">
+                        {/* <div className="flex justify-center w-full">
                             <a
                                 href="/rangkaian/ucare/volunteer"
                                 target="_blank"
@@ -68,6 +88,32 @@ function BodyUcare() {
                                     Closed
                                 </button>
                             </a>
+                        </div> */}
+                        <div className="sponsorUcare_Container">
+                            <img
+                                src={Sunpride_LYFE}
+                                alt="Sunpride_LYFE"
+                                className="sponsorutama"
+                            />
+                            <div className="px-4 pb-2 sponsorUcare">
+                                <img src={CiptaWarna} alt="CiptaWarna" />
+                                <img src={Deorex} alt="Deorex" />
+                                <img src={Evolene} alt="Evolene" />
+                                <img src={Asus} alt="Asus" />
+                                <img src={Fifgroup} alt="Fifgroup" />
+                                <img src={ITO_EN} alt="ITO_EN" />
+                                <img src={Leminerale} alt="Leminerale" />
+                                <img src={OCBC} alt="OCBC" />
+                                <img src={Pocari} alt="Pocari" />
+                                <img src={ROG} alt="ROG" />
+                                <img src={Roppang} alt="Roppang" />
+
+                                <img src={Sunpride} alt="Sunpride" />
+                                <img src={Teh_pucuk} alt="Teh_pucuk" />
+                                <img src={SariRoti} alt="SariRoti" />
+                                <img src={Tenjin} alt="Tenjin" />
+                                <img src={Vicmic} alt="Vicmic" />
+                            </div>
                         </div>
                     </div>
                 </div>
@@ -101,7 +147,7 @@ function BodyUcare() {
                         join forces to make a positive impact and spread
                         goodwill!"
                     </p>
-                    <div className="flex justify-center w-full">
+                    {/* <div className="flex justify-center w-full">
                         <a
                             href="/rangkaian/ucare/volunteer"
                             target="_blank"
@@ -115,6 +161,33 @@ function BodyUcare() {
                                 Closed
                             </button>
                         </a>
+                    </div> */}
+
+                    <div className="sponsorUcare_Container">
+                        <img
+                            src={Sunpride_LYFE}
+                            alt="Sunpride_LYFE"
+                            className="sponsorutama"
+                        />
+                        <div className="px-4 pb-2 sponsorUcare">
+                            <img src={CiptaWarna} alt="CiptaWarna" />
+                            <img src={Deorex} alt="Deorex" />
+                            <img src={Evolene} alt="Evolene" />
+                            <img src={Asus} alt="Asus" />
+                            <img src={Fifgroup} alt="Fifgroup" />
+                            <img src={ITO_EN} alt="ITO_EN" />
+                            <img src={Leminerale} alt="Leminerale" />
+                            <img src={OCBC} alt="OCBC" />
+                            <img src={Pocari} alt="Pocari" />
+                            <img src={ROG} alt="ROG" />
+                            <img src={Roppang} alt="Roppang" />
+
+                            <img src={Sunpride} alt="Sunpride" />
+                            <img src={Teh_pucuk} alt="Teh_pucuk" />
+                            <img src={SariRoti} alt="SariRoti" />
+                            <img src={Tenjin} alt="Tenjin" />
+                            <img src={Vicmic} alt="Vicmic" />
+                        </div>
                     </div>
                 </div>
             </div>
